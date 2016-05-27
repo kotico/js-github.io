2f6249a30ba96f3f5212366f638c4cc09867f41e
KatyaLukina <ekaterinalukina8@gmail.com>
2016-05-27 21:51:19 +0300

    first add

---
+++ Js_lesson_19_20/js/src/script.js
@@ -0,0 +1,29 @@
+$(function () {
+   
+   //slider
+
+    $('.flexslider').flexslider({
+    animation: "slide",
+    controlsContainer: $(".custom-controls-container"),
+  });
+  
+   //accordion
+
+   function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp().removeClass('open');
+    };
+ 
+    $('.accordion-section-tittle').click(function(e) {
+        var currentAttrValue = $(this).attr('href');
+        close_accordion_section();
+       
+            $(this).addClass('active');
+            $('.accordion-section-tittle').not($(this)).removeClass('active');
+            $('.accordion ' + currentAttrValue).slideToggle().addClass('open'); 
+       e.preventDefault();
+       });
+ 
+      
+
+})
